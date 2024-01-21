c2b0e01f0cf6c0ba795cf0092324f9f45ca9405d
gabriel.mcamargo <gabrielcamargo1409@gmail.com>
2024-01-21 20:11:25 -0300

    chore: list opening jobs

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,33 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return { hello: 'Hello World!' };
+    // faça um código que retorne um array de objetos de vagas de emprego
+
+    return [
+      {
+        id: 1,
+        title: 'Desenvolvedor Fullstack',
+        description: 'Desenvolvedor Fullstack com experiência em React e Node',
+        salary: 5000,
+        company: 'Google',
+        email: 'contact@google.com',
+      },
+      {
+        id: 2,
+        title: 'Desenvolvedor Frontend',
+        description: 'Desenvolvedor Frontend com experiência em React',
+        salary: 4000,
+        company: 'Facebook',
+        email: 'contact@facebook.com',
+      },
+      {
+        id: 3,
+        title: 'Desenvolvedor Backend',
+        description: 'Desenvolvedor Backend com experiência em Node',
+        salary: 4000,
+        company: 'Facebook',
+        email: '',
+      },
+    ];
   }
 }
